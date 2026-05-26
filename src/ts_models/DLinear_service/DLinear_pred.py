22b19dcd071f9a58cb1f868493096f17d79496de
Nikita <savvin.nikit@yandex.ru>
2026-05-26 19:03:40 +0300

    save

--- src/ts_models/DLinear_service/DLinear_pred.py
+++ src/ts_models/DLinear_service/DLinear_pred.py
@@ -214,115 +214,118 @@ def DLinear_forecast(
         logger,
         params=None
 ):
-    df_train = df_train.drop_duplicates(subset=[time_column])
-    df_test = df_test.drop_duplicates(subset=[time_column])
-
-    cfg = params or DEFAULT_DLINEAR_PARAMS
-
-    df_train = df_train.copy()
-    df_test = df_test.copy()
-
-    df_train[time_column] = pd.to_datetime(df_train[time_column])
-    df_test[time_column] = pd.to_datetime(df_test[time_column])
-
-    df_train = df_train.sort_values(time_column).reset_index(drop=True)
-    df_test = df_test.sort_values(time_column).reset_index(drop=True)
-
-    cols = [col_target] + list(col_for_train)
-    exog_cols = list(col_for_train)
-
-    df_train = df_train[cols + [time_column]]
-    df_test = df_test[cols + [time_column]]
-
-    df_train[col_target] = pd.to_numeric(df_train[col_target], errors="coerce")
-
-    freq = _infer_freq(df_train, time_column)
-
     try:
-        target_series = TimeSeries.from_dataframe(
+        df_train = df_train.drop_duplicates(subset=[time_column])
+        df_test = df_test.drop_duplicates(subset=[time_column])
+
+        cfg = params or DEFAULT_DLINEAR_PARAMS
+
+        df_train = df_train.copy()
+        df_test = df_test.copy()
+
+        df_train[time_column] = pd.to_datetime(df_train[time_column])
+        df_test[time_column] = pd.to_datetime(df_test[time_column])
+
+        df_train = df_train.sort_values(time_column).reset_index(drop=True)
+        df_test = df_test.sort_values(time_column).reset_index(drop=True)
+
+        cols = [col_target] + list(col_for_train)
+        exog_cols = list(col_for_train)
+
+        df_train = df_train[cols + [time_column]]
+        df_test = df_test[cols + [time_column]]
+
+        df_train[col_target] = pd.to_numeric(df_train[col_target], errors="coerce")
+
+        freq = _infer_freq(df_train, time_column)
+
+        try:
+            target_series = TimeSeries.from_dataframe(
+                df_train,
+                time_col=time_column,
+                value_cols=col_target,
+                fill_missing_dates=True,
+                freq=freq
+            ).astype(np.float32)
+        except Exception as e:
+            print(e)
+
+        past_cov = TimeSeries.from_dataframe(
             df_train,
             time_col=time_column,
-            value_cols=col_target,
+            value_cols=exog_cols,
             fill_missing_dates=True,
             freq=freq
         ).astype(np.float32)
-    except Exception as e:
-        print(e)
 
-    past_cov = TimeSeries.from_dataframe(
-        df_train,
-        time_col=time_column,
-        value_cols=exog_cols,
-        fill_missing_dates=True,
-        freq=freq
-    ).astype(np.float32)
-
-    model = DLinearModel(
-        input_chunk_length=lag,
-        output_chunk_length=cfg["output_chunk_length"],
-        n_epochs=cfg["n_epochs"],
-        batch_size=cfg["batch_size"],
-        random_state=SEED,
-        optimizer_kwargs={"lr": cfg["optimizer_lr"]},
-        pl_trainer_kwargs=_get_pl_kwargs()
-    )
-
-    model.fit(target_series, past_covariates=past_cov)
-
-    preds = []
-
-    history_target = target_series
-    history_cov = past_cov
-
-    last_time = target_series.time_index[-1]
-    step = pd.tseries.frequencies.to_offset(freq)
-
-    for i in range(len(df_test)):
-
-        pred = model.predict(
-            n=1,
-            series=history_target,
-            past_covariates=history_cov
+        model = DLinearModel(
+            input_chunk_length=lag,
+            output_chunk_length=cfg["output_chunk_length"],
+            n_epochs=cfg["n_epochs"],
+            batch_size=cfg["batch_size"],
+            random_state=SEED,
+            optimizer_kwargs={"lr": cfg["optimizer_lr"]},
+            pl_trainer_kwargs=_get_pl_kwargs()
         )
 
-        val = float(pred.values().ravel()[0])
-        preds.append(val)
+        model.fit(target_series, past_covariates=past_cov)
 
-        next_time = last_time + step
-        last_time = next_time
+        preds = []
 
-        new_target = pd.DataFrame({
-            time_column: [next_time],
-            col_target: [val]
-        })
+        history_target = target_series
+        history_cov = past_cov
 
-        new_target_ts = TimeSeries.from_dataframe(
-            new_target,
-            time_col=time_column,
-            value_cols=col_target,
-            fill_missing_dates=True,
-            freq=freq
-        ).astype(np.float32)
+        last_time = target_series.time_index[-1]
+        step = pd.tseries.frequencies.to_offset(freq)
 
-        history_target = history_target.append(new_target_ts)
+        for i in range(len(df_test)):
 
-        if past_cov is not None:
-            new_cov = pd.DataFrame({
+            pred = model.predict(
+                n=1,
+                series=history_target,
+                past_covariates=history_cov
+            )
+
+            val = float(pred.values().ravel()[0])
+            preds.append(val)
+
+            next_time = last_time + step
+            last_time = next_time
+
+            new_target = pd.DataFrame({
                 time_column: [next_time],
-                **{c: [df_test[c].iloc[i]] for c in exog_cols}
+                col_target: [val]
             })
 
-            new_cov_ts = TimeSeries.from_dataframe(
-                new_cov,
+            new_target_ts = TimeSeries.from_dataframe(
+                new_target,
                 time_col=time_column,
-                value_cols=exog_cols,
+                value_cols=col_target,
                 fill_missing_dates=True,
                 freq=freq
             ).astype(np.float32)
 
-            history_cov = history_cov.append(new_cov_ts)
+            history_target = history_target.append(new_target_ts)
 
-    return pd.DataFrame({
-        time_column: df_test[time_column].values,
-        col_target: preds
-    })
+            if past_cov is not None:
+                new_cov = pd.DataFrame({
+                    time_column: [next_time],
+                    **{c: [df_test[c].iloc[i]] for c in exog_cols}
+                })
+
+                new_cov_ts = TimeSeries.from_dataframe(
+                    new_cov,
+                    time_col=time_column,
+                    value_cols=exog_cols,
+                    fill_missing_dates=True,
+                    freq=freq
+                ).astype(np.float32)
+
+                history_cov = history_cov.append(new_cov_ts)
+
+        return pd.DataFrame({
+            time_column: df_test[time_column].values,
+            col_target: preds
+        })
+    except Exception as e:
+        print(e)
