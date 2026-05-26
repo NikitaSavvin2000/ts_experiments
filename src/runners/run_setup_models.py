6d7519bc92a4603b5fc50d1c046315142db4f5e0
Nikita <savvin.nikit@yandex.ru>
2026-05-26 14:18:22 +0300

    save

--- src/runners/run_setup_models.py
+++ src/runners/run_setup_models.py
@@ -79,6 +79,17 @@ df_ready_progress = load_and_prepare_progress(progress_csv_path=progress_csv_pat
 
 experiment_retest =  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQubvnzfbQFBoiy_Ag0rjS8G3GOX9Q3vFXf49Wf7jGEqPK7dcpjXdj67jRwJb6KdT5st2GpnvdMsvXn/pub?gid=688232135&single=true&output=csv"
 
+def to_list(x):
+    if isinstance(x, list):
+        return x
+    if pd.isna(x) or x == "":
+        return []
+    if isinstance(x, str):
+        return ast.literal_eval(x)
+    return []
+
+experiment_retest["additional_cols"] = experiment_retest["additional_cols"].apply(to_list)
+
 df_to_experiment = pd.read_csv(experiment_retest)
 
 print(df_to_experiment)
