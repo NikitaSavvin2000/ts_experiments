8b51c6e2fea4d068b1df9e074858afed5c8c4669
Nikita <savvin.nikit@yandex.ru>
2026-06-23 12:53:02 +0300

    save

--- src/runners/main_a.py
+++ src/runners/main_a.py
@@ -117,14 +117,14 @@ logger.info(msg["experiment_created"].format(experiment_design_path))
 
 
 #
-df_ready_progress = pd.read_csv(progress_csv_path)
-dataset_name_to_drop = ["russia_amur_region", "Temperature_in_Celsius"]
-df_ready_progress = df_ready_progress[~df_ready_progress["dataset_name"].isin(dataset_name_to_drop)]
-df_ready_progress.to_csv(progress_csv_path)
-
-print("WAS DROPPED!!!!!")
-print("WAS DROPPED!!!!!")
-print("WAS DROPPED!!!!!")
+# df_ready_progress = pd.read_csv(progress_csv_path)
+# dataset_name_to_drop = ["russia_amur_region", "Temperature_in_Celsius"]
+# df_ready_progress = df_ready_progress[~df_ready_progress["dataset_name"].isin(dataset_name_to_drop)]
+# df_ready_progress.to_csv(progress_csv_path)
+#
+# print("WAS DROPPED!!!!!")
+# print("WAS DROPPED!!!!!")
+# print("WAS DROPPED!!!!!")
 
 
 df_ready_progress = load_and_prepare_progress(
